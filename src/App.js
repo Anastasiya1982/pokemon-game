a6c6b73c6a8fc55d54b5ed71bea4b157c4ed9d1c
ACER <mahanasty@mail.ru>
2021-01-27 18:31:51 +0300

    homework2

--- src/App.js
+++ src/App.js
@@ -1,1 +1,36 @@
+import React from'react';
+import Header from "./components/Header/header";
+import Layout from "./components/Layout/Layout";
+import Footer from "./components/Footer/footer";
 
+
+
+const App=()=>{
+    return(
+        <>
+          <Header title="Title"
+          descr="This is the description of the header"/>
+           <Layout id="1"
+                    title="Layout1"
+                    descr="This is decription of the layout1"
+                    urlBg
+                   colorBg={false}
+
+           />
+           <Layout id="2"
+                   title="Layout2"
+                   descr="This is decription of the layout2"
+                   colorBg
+                   urlBg={false}
+           />
+           <Layout id="3"
+                   title="Layout3"
+                   descr="This is decription of the layout3"
+                   urlBg
+                   colorBg={false}
+                    />
+           <Footer />
+        </>
+    )
+}
+export default App;
---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import s from './style.module.css';
+
+const Layout = ({id, title, descr, urlBg=true, colorBg}) => {
+    return (
+        <section className={s.root} id={id}>
+            <div className={s.wrapper}>
+                <article>
+                    <div className={s.title}>
+                        <h3> {title}</h3>
+                        <span className={s.separator}></span>
+                    </div>
+                    <div className={s.desc + ' ' + s.full}>
+                        <p>{descr}</p>
+                    </div>
+                </article>
+            </div>
+        </section>
+    )
+}
+
+export default Layout;
