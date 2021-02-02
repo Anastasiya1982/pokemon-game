e54a0742e8d68640eaf0d8ac42ab55be59821eae
ACER <mahanasty@mail.ru>
2021-02-02 11:43:52 +0300

    добавили переход на станицу игры по клику на кнопку

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -4,13 +4,18 @@ import s from './style.module.css';
 
 // import  { ReactComponent as ReactLogo} from '../../assets/Pikachu-logo-D0AAA93F17-seeklogo.com.png';
 
-const Header=({ title, descr })=>{
+const Header=({ title, descr,onClickBtn })=>{
+    const handleClick=()=>{
+        onClickBtn&& onClickBtn('game');
+    }
+
     return(
         <header className={s.root}>
             <div className={s.forest}></div>
             <div className={s.container}>
                 <h1>{title}</h1>
                 <p>{descr}</p>
+                <button onClick={handleClick} className={s.btn}>Start  the Game</button>
             </div>
         </header>
     )
---
+++ src/routes/Game/game.js
@@ -0,0 +1,12 @@
+
+import React from 'react';
+import s from './style.module.css';
+
+const GamePage=()=>{
+    return(
+        <div className={s.wrapper}>
+            This is GAME PAGE
+        </div>
+    )
+};
+export default GamePage;
