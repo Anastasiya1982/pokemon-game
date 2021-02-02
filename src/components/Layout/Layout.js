fbc1158f7adc96fa08845609ac940f6bd421c32d
ACER <mahanasty@mail.ru>
2021-02-02 08:51:50 +0300

    подключили быблиотеку класснеймс

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import s from './style.module.css';
+import cn from 'classnames';
 
 
 const Layout = ({id, title, urlBg, colorBg,children}) => {
@@ -19,7 +20,7 @@ const Layout = ({id, title, urlBg, colorBg,children}) => {
                         <h3> {title}</h3>
                         <span className={s.separator}></span>
                     </div>
-                    <div className={s.desc + ' ' + s.full}>
+                    <div className={cn(s.desc , s.full)}>
                        {children}
                     </div>
                 </article>
--- src/components/PokemonCard/index.js
+++ src/components/PokemonCard/index.js
@@ -3,6 +3,8 @@ import React from 'react';
 import s from './style.module.css';
 import backCardImg from '../../assets/cardBackSide.jpg';
 import {useState} from 'react';
+import cn from 'classnames';
+
 
 const PokemonCard = ({ id, name, img, type, values }) => {
 
@@ -16,10 +18,10 @@ const PokemonCard = ({ id, name, img, type, values }) => {
 
     return (
         <div className={s.root} onClick={handleClick}>
-            <div className={`${s.pokemonCard} ${isActive? s.active:" "}`}>
+            <div className={cn(s.pokemonCard,{[s.active]:isActive})}>
                 <div className={s.cardFront}>
-                    <div className={`${s.wrap} ${s.front}`}>
-                        <div className={`${s.pokemon} ${s[type]}`}>
+                    <div className={cn(s.wrap,s.front)}>
+                        <div className={cn(s.pokemon,s[type])}>
                             <div className={s.values}>
                                 <div className={`${s.count} ${s.top}`}>{values.top}</div>
                                 <div className={`${s.count} ${s.right}`}>{values.right}</div>
@@ -39,7 +41,7 @@ const PokemonCard = ({ id, name, img, type, values }) => {
                 </div>
 
                 <div className={s.cardBack}>
-                    <div className={`${s.wrap}  ${s.back}`}>
+                    <div className={cn(s.wrap,s.back)}>
                         <img src={backCardImg} alt="Сard Backed"/>
                     </div>
                 </div>
